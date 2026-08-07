dfc24d572c4131da8eaf875e5dd9b01020b855a5
AlexandrPutenikhin <mr.alexander.wallis@protonmail.com>
2026-08-07 16:43:52 +0500

    Added basic entry point

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Penny
 Static website generator.
 
+# Dependencies
+
+- [Beatiful Soup 4](https://pypi.org/project/beautifulsoup4/)
+
 # License
 
 Penny is distributed under [GPL-3.0-only](./LICENSE) license.
--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,12 @@
+import sys
+
+from typing import Final
+from bs4 import BeautifulSoup
+
+USAGE: Final = "Usage: python3 penny.pyz <directory>; directory must contain index.html and index.py"
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    args = sys.argv
+    if len(args) < 2:
+        print(USAGE)
+        sys.exit(1)
