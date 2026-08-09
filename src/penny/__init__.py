d692a47977e3585416715375652fa8542ca269a4
AlexandrPutenikhin <mr.alexander.wallis@protonmail.com>
2026-08-09 14:42:11 +0500

    Fixed typo

--- src/penny/__init__.py
+++ src/penny/__init__.py
@@ -8,7 +8,7 @@ def exec_files(in_file: str, script_file: str) -> str:
     
     tag_regex = re.compile(r'pnn-[a-z\-]+')
     soup = BeautifulSoup(in_file, 'html.parser')
-    for tag in soup.find_all(teg_regex):
+    for tag in soup.find_all(tag_regex):
         fun_name = tag.name[4:].replace('-', '_')
         func = script_ns.get(fun_name)
         if callable(func):
