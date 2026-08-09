a4bce0cb64d0a283716bb3c47c3489e49023007e
AlexandrPutenikhin <mr.alexander.wallis@protonmail.com>
2026-08-09 14:25:08 +0500

    Finished writing exec_files function

--- src/penny/__init__.py
+++ src/penny/__init__.py
@@ -2,8 +2,16 @@ import re
 
 from bs4 import BeautifulSoup
 
-def exec_file(in_file: str, script_file: str):
-    tag_regex = re.compile(r'pnn-x-[a-z\-]+')
+def exec_files(in_file: str, script_file: str):
+    script_ns = {}
+    exec(script_file, script_ns)
+    
+    tag_regex = re.compile(r'pnn-[a-z\-]+')
     soup = BeautifulSoup(in_file, 'html.parser')
     for tag in soup.find_all(teg_regex):
-        fun_name = tag.name[6:].replace('-', '_')
+        fun_name = tag.name[4:].replace('-', '_')
+        func = script_ns.get(fun_name)
+        if callable(func):
+            tag.replace_with(func())
+        else:
+            tag.extract()
