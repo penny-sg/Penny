6968481224e1b463dcf63f818d332d5545b6372d
AlexandrPutenikhin <mr.alexander.wallis@protonmail.com>
2026-08-14 19:44:23 +0500

    FINAL REGEX CHANGE

--- src/penny/__init__.py
+++ src/penny/__init__.py
@@ -6,7 +6,7 @@ def exec_files(in_file: str, script_file: str) -> str:
     script_ns = {}
     exec(script_file, script_ns)
     
-    tag_regex = re.compile(r'^pnn-[a-z][a-z\-]*[a-z]$')
+    tag_regex = re.compile(r'^pnn-[a-z][a-z\-]*[a-z]?$')
     soup = BeautifulSoup(in_file, 'html.parser')
     for tag in soup.find_all(tag_regex):
         fun_name = tag.name[4:].replace('-', '_')
